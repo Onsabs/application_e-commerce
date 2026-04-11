46eb90cfb70fc3732f84b20831b229af0130ee2a
ons <onsabdelali@gmail.com>
2026-04-11 22:19:50 +0100

    modified sccs page home

--- front_angular/src/app/pages/home/home.component.ts
+++ front_angular/src/app/pages/home/home.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  slides = [
+  {
+    image: 'assets/images/gallery-02.jpg',
+    title: 'New Fashion Collection 2026',
+    desc: 'Découvrez les dernières tendances élégantes et modernes.'
+  },
+  {
+    image: 'assets/images/blog-02.jpg',
+    title: 'Premium Style',
+    desc: 'Des vêtements de haute qualité pour un look unique.'
+  },
+  {
+    image: 'assets/images/blog-03.jpg',
+    title: 'Limited Edition',
+    desc: 'Collection exclusive disponible pour une durée limitée.'
+  }
+];
+
+current = 0;
+
+ngOnInit() {
+  setInterval(() => {
+    this.current = (this.current + 1) % this.slides.length;
+  }, 4000);
+}
+
 
 }
